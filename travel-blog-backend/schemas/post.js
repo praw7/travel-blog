bc9c091ee676a5d752ff07009f0ceea8f22f9267
Hubert Zawisza <hubert.zawisza90@gmail.com>
2023-10-26 23:42:30 +0200

    removed leaflet, added posts to sanity

--- travel-blog-backend/schemas/post.js
+++ travel-blog-backend/schemas/post.js
@@ -1,35 +1,35 @@
-const getPosition = (options) => {
-  if (navigator.geolocation) {
-    return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(resolve, reject, options)
-    })
-  }
-}
+// const getPosition = (options) => {
+//   if (navigator.geolocation) {
+//     return new Promise((resolve, reject) => {
+//       navigator.geolocation.getCurrentPosition(resolve, reject, options)
+//     })
+//   }
+// }
 
 export default {
   name: 'post',
   type: 'document',
   title: 'Blog Post',
-  initialValue: async () => ({
-    postedAt: await getPosition()
-      .then(({coords}) => {
-        const {latitude, longitude, altitude} = coords
+  // initialValue: async () => ({
+  //   postedAt: await getPosition()
+  //     .then(({coords}) => {
+  //       const {latitude, longitude, altitude} = coords
 
-        return {
-          _type: 'geopoint',
-          lat: latitude,
-          lng: longitude,
-          alt: altitude || undefined,
-        }
-      })
-      .catch(() => undefined),
-  }),
+  //       return {
+  //         _type: 'geopoint',
+  //         lat: latitude,
+  //         lng: longitude,
+  //         alt: altitude || undefined,
+  //       }
+  //     })
+  //     .catch(() => undefined),
+  // }),
   fields: [
-    {
-      name: 'postedAt',
-      type: 'geopoint',
-      title: 'Location',
-    },
+    // {
+    //   name: 'postedAt',
+    //   type: 'geopoint',
+    //   title: 'Location',
+    // },
     {
       name: 'title',
       type: 'string',
